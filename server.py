8b9829b148d000f2e4f51a94405cfcc2323a08f1
Jeff Reimschussel <20570258+jtreim@users.noreply.github.com>
2020-04-27 12:50:02 -0600

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser
+from BaseHTTPServer import HTTPServer
+
+from settings import *
+from handlers.test import TestHandler
+
+
+def stop(server):
+    print('\nShutting down the server...')
+    server.socket.close()
+
+def start(port=PORT):
+    try:
+        # Create a web server and define the handler to manage the request
+        server = HTTPServer(('', port), TestHandler)
+        print('Start server on port {}'.format(port))
+
+        # Serve until told to stop
+        server.serve_forever()
+
+    except KeyboardInterrupt:
+        stop(server)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='Process server settings')
+    parser.add_argument('-p', '--port', type=int,
+                        required=False, help='Server port number')
+    args = parser.parse_args()
+
+    if args.port:
+        start(args.port)
+    else:
+        start()
