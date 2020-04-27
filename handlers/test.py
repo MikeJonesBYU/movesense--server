42b50f23334e85bc62317917cd3c370733e2d34a
jtreim <email.jreim@gmail.com>
2020-04-27 12:53:21 -0600

    Initial commit

---
+++ handlers/test.py
@@ -0,0 +1,9 @@
+from BaseHTTPServer import BaseHTTPRequestHandler
+
+class TestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write("Hello World!")
+        return
